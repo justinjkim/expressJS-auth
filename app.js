6995df7476415865e14d1ce4627c27d7153eaae5
Justin Kim <jjkim928@gmail.com>
2017-08-21 14:26:29 -0400

    fixed typo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustache = require('mustache-express');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 const data = require('./userData.js');
 //const adminRouter = require('./routes/admin.js');
 
@@ -14,11 +15,48 @@ app.set('views', ['./views', './views/admin']);
 // not gonna use router yet
 //app.use('/', adminRouter);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// not quite sure what this is for yet
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
+
+
+// amy's authenticate function
+function authenticate(req, username, password){
+  var authenticatedUser = data.users.find(function (user) {
+    if (username === user.username && password === user.password) {
+      req.session.authenticated = true;
+      console.log('User & Password Authenticated');
+    } else {
+      return false
+    }
+  });
+  console.log(req.session);
+  return req.session;
+}
+
+
 app.get('/', function(req, res) {
 	res.render("index.mustache");
-})
+});
 
+app.post('/', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  authenticate(req, username, password);
+  if (req.session && req.session.authenticated){
+    res.render('loggedin.mustache', { username: username });
+  } else {
+    res.redirect('/');
+  }
+});
 
 app.listen(3000, function() {
 	console.log("Starting up ExpressJS-login application...");
-})
+});
